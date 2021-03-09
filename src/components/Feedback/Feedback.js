357e0d3f6a0616a15822ca1409f1f8f39b99cfcb
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-09 23:16:04 +0200

    Update Feedback.js

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,24 +1,29 @@
-import React from "react";
-// import fs from "./Feedback.module.css";
-// import PropTypes from "prop-types";
+// import React from "react";
+// // import fs from "./Feedback.module.css";
+// // import PropTypes from "prop-types";
 
-const Feedback = ({ stats }) => {
-  console.dir(stats);
-  console.log(stats.caller());
-  const statsKeys = Object.keys(stats);
-  return (
-    <section>
-      <h2>Please leave feedback</h2>
-      <ul>
-        {statsKeys.map((stat) => {
-          return (
-            <li>
-              <button type="button">{stat}</button>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
-};
-export default Feedback;
+// const Feedback = ({ stats }) => {
+//   console.dir(stats);
+//   // console.log(stats.caller());
+
+//   const statsKeys = Object.keys(stats);
+//   return (
+//     <section>
+//       <ul>
+//         {statsKeys.map((stat) => {
+//           return (
+//             <li key={stat}>
+//               <button
+//                 type="button"
+//                 onClick={() => console.log(`кликнули в ${stat}`)}
+//               >
+//                 {stat}
+//               </button>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </section>
+//   );
+// };
+// export default Feedback;
