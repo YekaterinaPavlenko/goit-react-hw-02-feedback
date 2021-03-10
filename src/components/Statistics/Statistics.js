ebdc7723b6ea4bbead807970c9447562b586034f
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-10 02:10:35 +0200

    добавляет статистику

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,3 +1,34 @@
 import React from "react";
-import s from "./Statistics.module.css";
+// import s from "./Statistics.module.css";
 import PropTypes from "prop-types";
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  //   console.log(positivePercentage);
+  return (
+    <ul>
+      <li key="good">
+        <p>Good: {good}</p>
+      </li>
+      <li key="neutral">
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li key="bad">
+        <p>Bad: {bad}</p>
+      </li>
+      <li key="total">
+        <p>Total: {total}</p>
+      </li>
+      <li key="positive">
+        <p>Positive feedback: {positivePercentage}%</p>
+      </li>
+    </ul>
+  );
+};
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
+export default Statistics;
